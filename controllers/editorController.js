4082cfc2e6dc5b04dd20320970266fc085a25609
Tireostraz <ilagoyskiy@gmail.com>
2025-07-15 00:33:18 +0300

    template endpoint is in work

--- controllers/editorController.js
+++ controllers/editorController.js
@@ -10,24 +10,43 @@ export const loadPages = async (req, res) => {
   if (!userId && !guestId) {
     return res
       .status(404)
-      .json({ error: "Ошибка авторизации, нет userId или guestId" });
+      .json({ error: "Ошибка авторизации. Требуется userId или guestId" });
+  }
+
+  let project;
+
+  try {
+    const projectResult = await pool.query(
+      "SELECT * FROM projects WHERE id = $1",
+      [projectId]
+    );
+    project = projectResult.rows[0];
+  } catch (e) {
+    console.error("Ошибка при запросе проекта:", e);
+    return res
+      .status(500)
+      .json({ error: "Ошибка при получении проекта из БД" });
+  }
+
+  if (!project) {
+    return res.status(404).json({ error: "Проект не найден" });
+  }
+
+  if (project.user_id !== userId && project.guest_id !== guestId) {
+    return res.status(403).json({ error: "Нет доступа к проекту" });
   }
 
   //Смотрим, есть ли уже страницы данного проекта
-  const resultPages = await pool.query(
-    `SELECT pages.*, p.user_id, p.guest_id FROM pages
-    LEFT JOIN projects as p ON pages.project_id = p.id
-    LEFT JOIN editable_pictures ON editable_pictures.page_id = pages.id
-    LEFT JOIN editable_texts ON editable_texts.page_id = pages.id
-    WHERE (pages.project_id = $1)`,
+  const pagesResult = await pool.query(
+    "SELECT id, pages, print_quantities FROM pages WHERE pages.project_id = $1",
     [projectId]
   );
 
-  console.log(resultPages.rows);
+  const pages = pagesResult.rows[0];
 
   //Если нет, то берем заготовку из product_template definition
-  if (!resultPages.rows[0]) {
-    const result = await pool.query(
+  if (!pages) {
+    const definitionResult = await pool.query(
       `SELECT p.*, pr_t.* FROM projects as p 
     LEFT JOIN products as pr ON p.product_id = pr.id
     LEFT JOIN product_templates as pr_t ON pr.id = pr_t.product_id
@@ -35,17 +54,44 @@ export const loadPages = async (req, res) => {
       [projectId]
     );
 
-    if (
-      !result.rows[0] ||
-      !result.rows[0].definition ||
-      !result.rows[0].definition.pages
-    ) {
+    //Генерируем id для pages
+    const pagesId = crypto.randomUUID();
+
+    const definitions = definitionResult.rows[0];
+    console.log(definitions);
+
+    if (!definitions || !definitions.pages) {
       return res
         .status(404)
         .json({ error: "Не удалось загрузить шаблон проекта" });
     }
 
-    const formatedPages = result.rows[0].definition.pages.map((p) => {
+    //Формируем из строк запроса definitions ответ требуемого формата
+    const formatedDefinitions = {
+      id: pagesId,
+      definition: {
+        definition_version: definitions.definition_version,
+        //locale: definitions.locale, пока не нужно
+        pages: definitions.pages,
+        selection_photos: [],
+        gallery_photos: [],
+        template_type: definitions.template_type,
+        definition_version: definitions.definition_version,
+        status: definitions.status,
+        template_id: definitions.id, //Просто id данного product_templates
+      },
+    };
+
+    //Добавляем в formatedDefinitions массив объектов print_quantities если они есть в таблице product_templates (обычно только для template_type = prints)
+    if (definitions.print_quantities) {
+      formatedDefinitions.definition = {
+        ...formatedDefinitions.definition,
+        print_quantities: definitions.print_quantities,
+      };
+    }
+
+    //Удаляем из объекта(ов) pages поля editable_pictures: [] и editable_texts: [] чтобы добавть в pages - pages
+    const formatedPages = definitions.pages.map((p) => {
       return {
         key: p.key,
         color_key: p.color_key,
@@ -55,37 +101,150 @@ export const loadPages = async (req, res) => {
     });
 
     //и добавляем в страницы для дальнейшей работы
-    await pool.query(`INSERT INTO pages (project_id, pages) VALUES($1, $2)`, [
+    await pool.query(
+      `INSERT INTO pages (id, project_id, pages, print_quantities) VALUES($1, $2, $3, $4)`,
+      [
+        pagesId,
+        projectId,
+        JSON.stringify(formatedPages),
+        JSON.stringify(definitions.print_quantities),
+      ]
+    );
+
+    //аналогично добавляем данные в конкретный project из product_templates (его id и дальше для совместимости обращаемся к product_template по его id)
+    await pool.query(`UPDATE projects SET template_id = $1 WHERE id = $2`, [
+      definitions.id,
       projectId,
-      JSON.stringify(formatedPages),
     ]);
 
-    //дописать работу с editable_texts и pictures
-
-    /* await pool.query(`INSERT INTO editable_pictures ()`) */
-
-    const formatedContent = result.rows.map((p) => {
-      return {
-        id: p.id,
-        definition: p.definition,
-        definition_version: p.definition_version,
-        status: p.status,
-      };
-    });
-    return res.status(200).json({ response: formatedContent });
+    return res.status(200).json({ response: formatedDefinitions });
   }
 
-  //если страницы есть, проверяем владельца.
-  if (
-    resultPages.rows[0].user_id !== userId &&
-    resultPages.rows[0].guest_id !== guestId
-  ) {
-    return res.status(404).json({ error: "Ошибка авторизации" });
-  } else {
-    return res.status(200).json({ response: resultPages.rows });
-  }
+  //Иначе если есть страницы pages
+
+  const templateResult = await pool.query(
+    "SELECT * FROM product_templates WHERE id = $1",
+    [project.template_id]
+  );
+
+  const template = templateResult.rows[0];
+  //console.log("project", project);
+  //console.log("template by id", templateResult.rows);
+
+  //Ищем все editable_pictures, editable_texts, videos (потом если нужно будет)
+  const picturesResult = await pool.query(
+    "SELECT * FROM editable_pictures WHERE page_id = $1",
+    [pages.id]
+  );
+
+  const textsResult = await pool.query(
+    "SELECT * FROM editable_texts WHERE page_id = $1",
+    [pages.id]
+  );
+
+  console.log("pictures: ", picturesResult.rows);
+  console.log("texts: ", textsResult.rows);
+  console.log("pages:", pages);
+  pages.pages = pages.pages.map((page) => {
+    return {
+      ...page,
+      editable_pictures: picturesResult.rows,
+      editable_texts: textsResult.rows,
+    };
+  });
+
+  const formatedDefinitions = {
+    id: pages.id,
+    definition: {
+      definition_version: template.definition_version,
+      //locale: definitions.locale, пока не нужно
+      pages: pages.pages,
+      selection_photos: [], //TODO исправить. Далее если используем это
+      gallery_photos: [], //TODO исправить. Далее если используем это
+      template_type: template.template_type,
+      definition_version: template.definition_version,
+      status: project.status,
+      template_id: template.id,
+    },
+  };
+
+  res.status(200).json({ response: formatedDefinitions });
 };
 
-export const savePages = async (req, res) => {};
+//Запрос шаблона проекта 3.0/customizations/{{projectId}}/template
+export const loadTemplate = async (req, res) => {
+  const userId = req.user.user_id || null;
+  const guestId = req.guestId || null;
+  const projectId = req.params.projectId;
 
-export const loadTemplate = async (req, res) => {};
+  if (!userId && !guestId) {
+    return res
+      .status(404)
+      .json({ error: "Ошибка авторизации. Требуется userId или guestId" });
+  }
+
+  let project;
+
+  try {
+    const result = await pool.query("SELECT * FROM projects WHERE id = $1", [
+      projectId,
+    ]);
+    project = result.rows[0];
+  } catch (e) {
+    console.error("Ошибка получения проектов", e);
+    return res.status(500).json({ error: "Ошибка при получении проектов" });
+  }
+
+  let template;
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM product_templates WHERE id = $1",
+      [project.template_id]
+    );
+    template = result.rows[0];
+  } catch (e) {
+    console.error("Ошибка при получении шаблона проекта из БД", e);
+    return res
+      .status(500)
+      .json({ error: "Ошибка при получении шаблона проекта из БД" });
+  }
+
+  let template_pages;
+
+  try {
+    const result = await pool.query(
+      "SELECT category_key, filter_type_keys, height_dmm, width_dmm, key, type, color_keys, elements FROM template_pages WHERE template_id = $1",
+      [template.id]
+    );
+    template_pages = result.rows;
+  } catch (e) {
+    console.error("Ошибка получения шаблонов страниц из БД", e);
+  }
+
+  const formatedTemplate = {
+    id: template.id,
+    definition: {
+      page_configuration: {
+        filter_type_definitions: template.filter_type_definitions,
+        force_pack_color: template.force_pack_color,
+        link_in_content: template.link_in_content,
+        increment: template.increment,
+        forced_layouts_filter_type_keys:
+          template.forced_layouts_filter_type_keys,
+        next_filter_type_keys: template.next_filter_type_keys,
+      },
+      definition_version: template.definition_version,
+      min_dpi: template.min_dpi,
+      template_type: template.template_type,
+    },
+    page_definitions: template_pages,
+  };
+
+  res.status(200).json({ response: formatedTemplate });
+};
+
+export const savePages = async (req, res) => {
+  const userId = req.user.user_id || null;
+  const guestId = req.guestId || null;
+};
--- routes/editor.js
+++ routes/editor.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { authenticateToken } from "../middleware/authMiddleware.js";
-import { loadPages } from "../controllers/editorController.js";
+import { loadPages, loadTemplate } from "../controllers/editorController.js";
 
 const router = express.Router();
 router.use(authenticateToken);
 
 router.get("/:projectId/content", loadPages);
+
+router.get("/:projectId/template", loadTemplate);
 
 export default router;
